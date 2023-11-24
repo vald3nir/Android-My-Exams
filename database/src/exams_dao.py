22b05a8a3af52d2dcc7afe247621e4f1e6c1fd53
Valdenir Severino <vald3nir@gmail.com>
2023-11-24 18:33:16 -0300

    update keys

--- database/src/exams_dao.py
+++ database/src/exams_dao.py
@@ -1,4 +1,6 @@
-from config import FIREBASE_CONFIG, FIREBASE_CERTIFICATE
+import json
+
+from keys.saude_secret import FIREBASE_CONFIG, FIREBASE_CERTIFICATE
 from toolkit.database.clients.firebase import Firebase
 from toolkit.utils.backup_utils import load_list_from_csv, save_list_to_csv
 
@@ -6,7 +8,7 @@ from toolkit.utils.backup_utils import load_list_from_csv, save_list_to_csv
 class ExamsDAO(Firebase):
 
     def __init__(self) -> None:
-        super().__init__(FIREBASE_CONFIG, FIREBASE_CERTIFICATE)
+        super().__init__(firebase_config=FIREBASE_CONFIG, firebase_certificate=FIREBASE_CERTIFICATE)
 
     def restore_db_from_dataset(self, path: str, dataset_path: str):
         _data = load_list_from_csv(dataset_path)
